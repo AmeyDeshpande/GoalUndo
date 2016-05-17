138c79a67fd11258410ccca62f6e8b62fac48b87
Amey Deshpande <ameyadeshpande90@gmail.com>
2016-05-17 11:59:57 -0700

    Test undoOperation()

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -80,4 +80,53 @@ TEST(GoalUndoTest, test_getOperations2)
 	test.addOperation	(op2);
 	ASSERT_EQ(test.getOperations(),resultant_op);
 }
+TEST(GoalUndoTest, test_undoOperation)
+{
+	GoalUndo test;
+	string op = "newoperation";
+	string goal = "newgoal";
+	string op2 = "anotheroperation";
+	string resultant_op = "newoperation anotheroperation";
+	test.addOperation(goal,op);
+	test.addOperation	(op2);
+	ASSERT_EQ(test.getOperations(),resultant_op);
+	test.undoOperation();
+	ASSERT_EQ(test.getOperations(),op);
+	ASSERT_EQ(test.getOperations(),op2);
+}
+TEST(GoalUndoTest, test_undoOperation2)
+{
+	GoalUndo test;
+	string op = "newoperation";
+	string goal = "newgoal";
+	test.addOperation(goal,op);
+	test.undoOperation();
+	ASSERT_EQ(test.getOperations(),"");
+}
+TEST(GoalUndoTest, test_undoOperation3)
+{
+	GoalUndo test;
+	string op = "newoperation";
+	string goal = "newgoal";
+	test.addOperation(goal,op);
+	test.undoOperation();
+	test.undoOperation();
+	ASSERT_EQ(test.getOperations(),"");
+}
+TEST(GoalUndoTest, test_undoOperationMultipleOperations)
+{
+	GoalUndo test;
+	string op = "newoperation";
+	string goal = "newgoal";
+	string op2 = "anotheroperation";
+	string op3 = "operation3";
+	string resultant_op = "newoperation anotheroperation operation3";
+	string resultant_op2 = "newoperation anotheroperation";
+	test.addOperation(goal,op);
+	test.addOperation(op2);
+	test.addOperation(op3);
+	ASSERT_EQ(test.getOperations(),resultant_op);
+	test.undoOperation();
+	ASSERT_EQ(test.getOperations(),resultant_op2);
+}
 
